29603610d309d9d43bf3b71b611e1222f7ba2906
Lydiah Kiboi <118690917+Lydiah6470@users.noreply.github.com>
2023-03-07 19:39:13 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-for i in range(97, 123):
-    if chr(i) not in "qe":
-        print(chr(i), end="")
+for a in range(ord('a'), ord('z') + 1):
+    if chr(a) != 'e' and chr(a) != 'q':
+        print("{:c}".format(a), end='')

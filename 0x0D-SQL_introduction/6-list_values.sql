f8a2e821785f35d2c016df564f6f1d933ef1cc65
Lydiah Kiboi <starlight141524@gmail.com>
2023-05-17 01:38:09 +0300

    changes

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
 -- lists all rows of the table first_table from the database 
-SHOW * FROM 'first_table';
+SELECT * FROM 'first_table';
--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
---nserts a new row in the table first_table
-INSERT INTO 'first_table' VALUES 'id=89 name='Best School'';
- 
+-- Inserts a new row into the table first_table.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School")

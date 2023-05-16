f9c902a10ace7f9c0bd485f0cef38cf5062788fb
Lydiah Kiboi <starlight141524@gmail.com>
2023-05-16 22:43:36 +0300

    delete

--- 0x0D-SQL_introduction/10-top_score.sql
+++
@@ -1,2 +0,0 @@
---lists all records of the table second_table of the database hbtn_0c_0
-SHOW TABLE 'second_table';
--- 0x0D-SQL_introduction/6-list_values.sql
+++
@@ -1,2 +0,0 @@
--- lists all rows of the table first_table from the database 
-SHOW * FROM 'first_table';
